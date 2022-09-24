6ae332926b782398f3267604f42fbf956ca3358b
scotttyso <tyscott@cisco.com>
2022-09-24 16:04:06 -0400

    first commit

---
+++ main.tf
@@ -0,0 +1,108 @@
+#____________________________________________________________
+#
+# Intersight Organization Data Source
+# GUI Location: Settings > Settings > Organizations > {Name}
+#____________________________________________________________
+
+data "intersight_organization_organization" "org_moid" {
+  name = var.organization
+}
+
+#____________________________________________________________
+#
+# Intersight UCS Server Profile(s) Data Source
+# GUI Location: Profiles > UCS Server Profiles > {Name}
+#____________________________________________________________
+
+data "intersight_server_profile" "profiles" {
+  for_each = { for v in var.profiles : v.name => v if v.object_type == "server.Profile" }
+  name     = each.value.name
+}
+
+#__________________________________________________________________
+#
+# Intersight UCS Server Profile Template(s) Data Source
+# GUI Location: Templates > UCS Server Profile Templates > {Name}
+#__________________________________________________________________
+
+data "intersight_server_profile_template" "templates" {
+  for_each = { for v in var.profiles : v.name => v if v.object_type == "server.ProfileTemplate" }
+  name     = each.value.name
+}
+
+#__________________________________________________________________
+#
+# Intersight SD Card Policy
+# GUI Location: Policies > Create Policy > SD Card
+#__________________________________________________________________
+
+resource "intersight_sdcard_policy" "sd_card" {
+  depends_on = [
+    data.intersight_server_profile.profiles,
+    data.intersight_server_profile_template.templates,
+    data.intersight_organization_organization.org_moid
+  ]
+  description = var.description != "" ? var.description : "${var.name} SD Card Policy."
+  name        = var.name
+  organization {
+    moid        = data.intersight_organization_organization.org_moid.results[0].moid
+    object_type = "organization.Organization"
+  }
+  dynamic "partitions" {
+    for_each = { for v in [var.enable_os]: v => v if var.enable_os == true }
+    content {
+      type        = "OS"
+      object_type = "sdcard.Partition"
+      virtual_drives {
+        additional_properties = jsonencode({ Name = "Hypervisor" })
+        enable                = var.enable_os
+        object_type           = "sdcard.OperatingSystem"
+      }
+    }
+  }
+  dynamic "partitions" {
+    for_each = {
+      for v in [var.enable_diagnostics] : v => v if length(
+        regexall(true, var.enable_diagnostics)) > 0 || length(
+        regexall(true, var.enable_drivers)) > 0 || length(
+        regexall(true, var.enable_huu)) > 0 || length(
+        regexall(true, var.enable_scu)) > 0
+    }
+    content {
+      type        = "Utility"
+      object_type = "sdcard.Partition"
+      virtual_drives {
+        enable      = var.enable_diagnostics
+        object_type = "sdcard.Diagnostics"
+      }
+      virtual_drives {
+        enable      = var.enable_drivers
+        object_type = "sdcard.Drivers"
+      }
+      virtual_drives {
+        enable      = var.enable_huu
+        object_type = "sdcard.HostUpgradeUtility"
+      }
+      virtual_drives {
+        enable      = var.enable_scu
+        object_type = "sdcard.ServerConfigurationUtility"
+      }
+    }
+  }
+  dynamic "profiles" {
+    for_each = { for v in var.profiles : v.name => v }
+    content {
+      moid = length(regexall("server.ProfileTemplate", profiles.value.object_type)
+        ) > 0 ? data.intersight_server_profile_template.templates[profiles.value.name].results[0
+      ].moid : data.intersight_server_profile.profiles[profiles.value.name].results[0].moid
+      object_type = profiles.value.object_type
+    }
+  }
+  dynamic "tags" {
+    for_each = var.tags
+    content {
+      key   = tags.value.key
+      value = tags.value.value
+    }
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,125 @@
+#____________________________________________________________
+#
+# SD Card Policy Variables Section.
+#____________________________________________________________
+
+variable "description" {
+  default     = ""
+  description = "Description for the Policy."
+  type        = string
+}
+
+variable "enable_diagnostics" {
+  default     = false
+  description = <<-EOT
+    Flag to Enable the Diagnostics Utility Partition.
+    * When two cards are present in the Cisco FlexFlash controller and Operating System is chosen in the SD card policy, the configured OS partition is mirrored. If only single card is available in the Cisco FlexFlash controller, the configured OS partition is non-RAID. The utility partitions are always set as non-RAID.
+    * Note:
+      - This policy is currently not supported on M6 servers.
+      - You can enable up to two utility virtual drives on M5 servers, and any number of supported utility virtual drives on M4 servers.
+      - Diagnostics is supported only for the M5 servers.
+      - UserPartition drives can be renamed only on the M4 servers.
+      - FlexFlash configuration is not supported on C460 M4 servers.
+      - For the Operating System+Utility mode, the M4 servers require two FlexFlash cards, and the M5 servers require at least 1 FlexFlash + 1 FlexUtil card.
+  EOT
+  type        = bool
+}
+
+variable "enable_drivers" {
+  default     = false
+  description = <<-EOT
+    Flag to Enable the Drivers Utility Partition.
+    * When two cards are present in the Cisco FlexFlash controller and Operating System is chosen in the SD card policy, the configured OS partition is mirrored. If only single card is available in the Cisco FlexFlash controller, the configured OS partition is non-RAID. The utility partitions are always set as non-RAID.
+    * Note:
+      - This policy is currently not supported on M6 servers.
+      - You can enable up to two utility virtual drives on M5 servers, and any number of supported utility virtual drives on M4 servers.
+      - Diagnostics is supported only for the M5 servers.
+      - UserPartition drives can be renamed only on the M4 servers.
+      - FlexFlash configuration is not supported on C460 M4 servers.
+      - For the Operating System+Utility mode, the M4 servers require two FlexFlash cards, and the M5 servers require at least 1 FlexFlash + 1 FlexUtil card.
+  EOT
+  type        = bool
+}
+
+variable "enable_huu" {
+  default     = false
+  description = <<-EOT
+    Flag to Enable the HostUpgradeUtility Utility Partition.
+    * When two cards are present in the Cisco FlexFlash controller and Operating System is chosen in the SD card policy, the configured OS partition is mirrored. If only single card is available in the Cisco FlexFlash controller, the configured OS partition is non-RAID. The utility partitions are always set as non-RAID.
+    * Note:
+      - This policy is currently not supported on M6 servers.
+      - You can enable up to two utility virtual drives on M5 servers, and any number of supported utility virtual drives on M4 servers.
+      - Diagnostics is supported only for the M5 servers.
+      - UserPartition drives can be renamed only on the M4 servers.
+      - FlexFlash configuration is not supported on C460 M4 servers.
+      - For the Operating System+Utility mode, the M4 servers require two FlexFlash cards, and the M5 servers require at least 1 FlexFlash + 1 FlexUtil card.
+  EOT
+  type        = bool
+}
+
+variable "enable_os" {
+  default     = false
+  description = <<-EOT
+    Flag to Enable the OperatingSystem Partition.
+    * When two cards are present in the Cisco FlexFlash controller and Operating System is chosen in the SD card policy, the configured OS partition is mirrored. If only single card is available in the Cisco FlexFlash controller, the configured OS partition is non-RAID. The utility partitions are always set as non-RAID.
+    * Note:
+      - This policy is currently not supported on M6 servers.
+      - You can enable up to two utility virtual drives on M5 servers, and any number of supported utility virtual drives on M4 servers.
+      - Diagnostics is supported only for the M5 servers.
+      - UserPartition drives can be renamed only on the M4 servers.
+      - FlexFlash configuration is not supported on C460 M4 servers.
+      - For the Operating System+Utility mode, the M4 servers require two FlexFlash cards, and the M5 servers require at least 1 FlexFlash + 1 FlexUtil card.
+  EOT
+  type        = bool
+}
+
+variable "enable_scu" {
+  default     = false
+  description = <<-EOT
+    Flag to Enable the ServerConfigurationUtility Utility Partition.
+    * When two cards are present in the Cisco FlexFlash controller and Operating System is chosen in the SD card policy, the configured OS partition is mirrored. If only single card is available in the Cisco FlexFlash controller, the configured OS partition is non-RAID. The utility partitions are always set as non-RAID.
+    * Note:
+      - This policy is currently not supported on M6 servers.
+      - You can enable up to two utility virtual drives on M5 servers, and any number of supported utility virtual drives on M4 servers.
+      - Diagnostics is supported only for the M5 servers.
+      - UserPartition drives can be renamed only on the M4 servers.
+      - FlexFlash configuration is not supported on C460 M4 servers.
+      - For the Operating System+Utility mode, the M4 servers require two FlexFlash cards, and the M5 servers require at least 1 FlexFlash + 1 FlexUtil card.
+  EOT
+  type        = bool
+}
+
+variable "name" {
+  default     = "default"
+  description = "Name for the Policy."
+  type        = string
+}
+
+variable "organization" {
+  default     = "default"
+  description = "Intersight Organization Name to Apply Policy to.  https://intersight.com/an/settings/organizations/."
+  type        = string
+}
+
+variable "profiles" {
+  default     = []
+  description = <<-EOT
+    List of Profiles to Assign to the Policy.
+    * name - Name of the Profile to Assign.
+    * object_type - Object Type to Assign in the Profile Configuration.
+      - server.Profile - For UCS Server Profiles.
+      - server.ProfileTemplate - For UCS Server Profile Templates.
+  EOT
+  type = list(object(
+    {
+      name        = string
+      object_type = optional(string, "server.Profile")
+    }
+  ))
+}
+
+variable "tags" {
+  default     = []
+  description = "List of Tag Attributes to Assign to the Policy."
+  type        = list(map(string))
+}
